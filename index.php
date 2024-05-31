825ec56f273c564c126f0984f5e54d4fcd8f64a2
ilex1112 <68603173+ilex1112@users.noreply.github.com>
2024-05-31 23:07:03 +0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ spl_autoload_register();
 
 
 
-echo (new Db('test1', 'users'))->select_id(1)['name'];
+echo (new Db('test1', 'users'))->select_id(1)['name']; //Вывод имени пользователя из базы данных по id = 1, через анонимный обьект.
 
 
 
