eada1cdd0a627e82a5cc5f9e8240057554c09087
ilex1112 <68603173+ilex1112@users.noreply.github.com>
2024-05-31 23:00:34 +0400

    Update Db.php

--- Db.php
+++ Db.php
@@ -4,11 +4,13 @@
 
 class Db
 {   
-    public $db;
+    private $db;
+    private $table;
 
-    public function __construct($db)
+    public function __construct($db, $table)
     {
         $this->db = $db;
+        $this->table = $table;
     }
 
     //Подключение к mysql
@@ -34,38 +36,38 @@ class Db
     }
 
     //Создание таблицы в базе данных
-    public function create_table($table, $column){
-        $sql = "CREATE TABLE $table $column;";
+    public function create_table($column){
+        $sql = "CREATE TABLE $this->table $column;";
         return $this->connect_table()->exec($sql);
     }
 
     //Добавление данных в таблицу (id, name, pass) VALUES (1, \'ilex\', 12345)
-    public function insert_table($table, $column){
-        $sql = "INSERT INTO $table $column;";
+    public function insert_table($column){
+        $sql = "INSERT INTO $this->table $column;";
         return $this->connect_table()->exec($sql);
     }
 
     //Извлечение всех данных из таблицы через переменную с методом fetch
-    public function select($table){
-        $sql = "SELECT * FROM $table";
+    public function select(){
+        $sql = "SELECT * FROM $this->table";
         return $this->connect_table()->query($sql);
     }
 
     //Извлечение 1 данных из таблицы через переменную
-    public function select_id($table, $id){
-        $sql = "SELECT * FROM $table WHERE id = $id";
+    public function select_id($id){
+        $sql = "SELECT * FROM $this->table WHERE id = $id";
         return $this->connect_table()->query($sql)->fetch();
     }
 
     //Обновление данных в таблице для конкретного id, в формате $column = (name = \'ilex\')
-    public function update($table, $column, $id){
-        $sql = "UPDATE $table SET $column WHERE id = $id";
+    public function update($column, $id){
+        $sql = "UPDATE $this->table SET $column WHERE id = $id";
         return $this->connect_table()->exec($sql);
     }
 
     //Удаленние данных из таблицы по id
-    public function delete($table, $id){
-        $sql = "DELETE FROM $table WHERE id = $id";
+    public function delete($id){
+        $sql = "DELETE FROM $this->table WHERE id = $id";
         return $this->connect_table()->exec($sql);
     }
 }
